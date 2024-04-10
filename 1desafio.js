3a5999edb73305a1a85bc403876336ec5e2dc6f0
thz2501 <160233763+thz2501@users.noreply.github.com>
2024-04-10 18:03:13 -0300

    mudança 3
    12345

---
+++ 1desafio.js
@@ -0,0 +1,3 @@
+const valor = 12
+if(valor >5){    
+console.log(`quando o ${valor} for maior que 5 `)}                                                        
--- calculadora.js
+++ calculadora.js
@@ -1,3 +1,10 @@
 const notaAluno01 = 9.8
 const notaAluno02 = 10 
 const notaAluno03 = 2
+const media = (notaAluno01 + notaAluno02 + notaAluno03)/3
+console.log(media)
+if(media >= 5){    
+    console.log(`a turma foi aprovada com a media de ${media}`)}   
+    
+    else{
+    console.log(`a turma foi reprovada com a media de ${media}`)}
