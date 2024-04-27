919f3e2677d576c5cc93dc86258468e4fabb4c5f
thz2501 <160233763+thz2501@users.noreply.github.com>
2024-04-26 21:47:21 -0300

    novo 2
    abc

---
+++ for.js
@@ -0,0 +1,24 @@
+const construtora = [
+    {
+    profissao: "pedreiro",
+    quantidade: 10,
+    turno: "5 de manhã/ 5 de tarde"},
+    {
+    profissao:"engenheiro",
+    quantidade: 3,
+    turno: "2 de manhã/1 de tarde"},
+    {
+    profissao: "carpinteiro",
+    quantidade: 6,
+    turno: '4 de manhã/2 de tarde'},
+    {
+    profissao: "metalúrgico",
+    quantidade: 3,
+    turno: "tarde"},
+    {
+    profissao: "servente",
+    quantidade: 10,
+    turno: "5 de manhã/5 de tarde"},
+    ]
+    for (let i = 0 ; i<10; i++)
+    console.log(i)
